cc1a2b27157d637b26b06bedfef885c1e9d990e6
Hugo Bjork <hugobjork@me.com>
2022-07-02 18:50:42 +0200

    finishing touch

---
+++ src/MovieCard.jsx
@@ -0,0 +1,31 @@
+
+export const MovieCard = ({ movie }) => {
+  return (
+<div className="movie">
+          <div>
+            <p>{movie.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie.Poster !== "N/A"
+                  ? movie.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie.Title}
+            />
+          </div>
+          <div>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
+          </div>
+        </div>
+  )
+}
+
+export default MovieCard;
+
+
+
+//there isnt any difference between a js and jsx file, 
+// you could use jsx for components
